7d1c495d630dc6174693ea0ab13e446c138dd89c
Mid 2014 Macbook Retina <mid2014macbookretina@airbears2-10-142-84-35.airbears2.1918.berkeley.edu>
2018-02-08 18:25:27 -0800

    everything works!

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+//Friends JSON file to be appended to.
+var friends = [
+  {
+    name: "Glo Austin",
+    photo: "https://media-exp2.licdn.com/mpr/mpr/shrinknp_400_400/AAMABADGAAwAAQAAAAAAAA9NAAAAJDgwMGViMWQ3LTFkNDQtNDYzZS1hYTZiLWVjZTY5M2RhODY2NQ.jpg",
+    scores: [
+      2,
+      3,
+      4,
+      5,
+      4,
+      3,
+      2,
+      4,
+      3,
+      2
+    ]
+  },
+
+  {
+    name: "Rachel Herndon",
+    photo: "#",
+    scores: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      1,
+      2,
+      4,
+      3,
+      2
+    ]
+  },
+
+  {
+    name: "Rachel Herndon",
+    photo: "#",
+    scores: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      1,
+      2,
+      4,
+      3,
+      2
+    ]
+  }
+];
+
+  module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,46 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+var userInput = require("../data/friends");
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+  app.get("/api/friends", function(req, res) {
+    res.json(userInput);
+  });
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the userInput array)
+  // ---------------------------------------------------------------------------
+  app.post("/api/friends", function(req, res) {
+    var userData = req.body
+    console.log('userScores = ' + JSON.stringify(userData));
+
+    var userScores = userData.scores;
+    var matchName = '';
+    var matchImage = '';
+    var comparison = 1000
+
+    //loop through friends objects
+    for (var i = 0; i < friends.length; i++) {
+      var difference = 0
+      for (var j = 0; j < userScores.length; j++) {
+        difference += Math.abs(friends[i].scores[j] - userScores[j]);
+      }
+      console.log('difference = ', difference);
+    }
+  });
+
+
+};
