ddaf0ea059336af9f563d537611973182d41f182
Mid 2014 Macbook Retina <mid2014macbookretina@mids-mbp.lan>
2018-03-04 18:07:35 -0800

    fixed variable in apiroutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,7 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-var userInput = require("../data/friends");
+var friends = require("../data/friends");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -14,14 +14,14 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
   app.get("/api/friends", function(req, res) {
-    res.json(userInput);
+    res.json(friends);
   });
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the userInput array)
+  // (ex. User fills out survey... this data is then sent to the server...
+  // Then the server saves the data to the friends Array)
   // ---------------------------------------------------------------------------
   app.post("/api/friends", function(req, res) {
     var userData = req.body
